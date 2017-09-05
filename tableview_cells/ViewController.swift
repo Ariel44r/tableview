fbe87b8e5a5df3ac473f7743af5c9945cade8c02
Ariel Ramírez <aramirez@iMac-de-Armit.local>
2017-09-05 02:30:39 -0500

    _first_commit_on_table_view_cells_simple_app_

--- tableview_cells/ViewController.swift
+++ tableview_cells/ViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let list = ["enchiladas","tacos","Gorditas"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (list.count)
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return (cell)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
