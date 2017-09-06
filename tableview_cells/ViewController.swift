c06ef3507094f93b7ab8d305399d2382625a166e
Ariel Ramírez <aramirez@iMac-de-Armit.local>
2017-09-06 17:47:25 -0500

    _add_swipe_to_delete_cell_

--- tableview_cells/ViewController.swift
+++ tableview_cells/ViewController.swift
@@ -10,18 +10,53 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    let list = ["Ernesto","Gabriel","Karla"]
+    var list = ["Ernesto","Gabriel","Karla"]
     
+    //return_number_cells
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (list.count)
     }
     
+    //custom_cell
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! viewcontrollerTableViewCell
         cell.myImage.image = UIImage(named:(list[indexPath.row] + ".jpg"))
         cell.myLabel.text = list[indexPath.row]
         return (cell)
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
+    {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == .delete
+        {
+            list.remove(at: indexPath.row)
+            tableView.reloadData()
+            print(list)
+        }
+    }
+    
+    @IBAction func button_borrar(_ sender: Any) {
+        
+        displayalert(userMessage: "Seguro que desea borrar el elemento de la lista?")
+        
+    }
+    
+    
+    //display_alert_function
+    func displayalert(userMessage:String) {
+        let myalert = UIAlertController(title:"Aviso", message:userMessage, preferredStyle: UIAlertControllerStyle.alert)
+        let okAction = UIAlertAction(title:"ok", style: UIAlertActionStyle.default, handler:nil)
+        myalert.addAction(okAction)
+        let declineaction = UIAlertAction(title:"decline", style: UIAlertActionStyle.default, handler:nil)
+        myalert.addAction(declineaction)
+        self.present(myalert, animated:true, completion:nil)
+    }
+
     
     
     override func viewDidLoad() {
--- tableview_cells/viewcontrollerTableViewCell.swift
+++ tableview_cells/viewcontrollerTableViewCell.swift
@@ -11,8 +11,7 @@ import UIKit
 class viewcontrollerTableViewCell: UITableViewCell {
     
     @IBOutlet weak var myImage: UIImageView!
-    @IBOutlet weak var myLabel: UILabel!
-   
+    @IBOutlet weak var myLabel: UILabel!   
     
     
     
